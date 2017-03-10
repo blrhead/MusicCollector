90140cf811740c972a44e9a16f0834f108c1b0ad
Daniel Bonaparte <danielbonaparte@Daniels-MacBook-Pro.local>
2017-03-10 09:04:30 -0800

    Added photo to window

---
+++ MusicCollector/MusicViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MusicViewController.swift
+//  MusicCollector
+//
+//  Created by Daniel Bonaparte on 3/10/17.
+//  Copyright © 2017 bonapARTe. All rights reserved.
+//
+
+import UIKit
+
+class MusicViewController: UIViewController {
+    
+    @IBOutlet var titleTextField: UITextField!
+    
+    @IBOutlet var musicImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: AnyObject) {
+    }
+ 
+    @IBAction func cameraTapped(_ sender: AnyObject) {
+    }
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+
+}
